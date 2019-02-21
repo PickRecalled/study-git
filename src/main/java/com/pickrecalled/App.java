b009494f4cabf334ae699ed8e0cb9719b0b20bfd
pickrecalled <pickrecalled@163.com>
2019-02-21 08:54:14 +0800

    add minusMethod

--- src/main/java/com/pickrecalled/App.java
+++ src/main/java/com/pickrecalled/App.java
@@ -2,6 +2,24 @@ package com.pickrecalled;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		addMethod(20.0, 30.0, 40.0, 50.0);
+		minusMethod(20.0, 30.0, 40.0, 50.0);
 	}
+
+	public static void addMethod(double... num) {
+		double v = 0.0D;
+		for (double n : num) {
+			v += n;
+		}
+		System.out.println("num:" + v);
+	}
+
+	public static void minusMethod(double... num) {
+		double v = 500.0D;
+		for (double n : num) {
+			v -= n;
+		}
+		System.out.println("num:" + v);
+	}
+
 }
